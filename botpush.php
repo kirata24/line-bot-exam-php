e4facfb312207a2e49e207b144b8f6748f8c5fa4
kirata24 <45958137+kirata24@users.noreply.github.com>
2018-12-18 13:26:44 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -9,16 +9,19 @@ $access_token = 'Kmv7HHpBjJRKhGrxDRneUMAM4PCnb1NBKUSAttLii+UmtS0KZ41WjEsw7Ua9/K+
 $channelSecret = '3f7c7b6a3087ddb8d0a4e8c7a9c29c22';
 
 $pushID = 'U2681897e58906f5f56ee6cff6f287f32';
+$pushID2 = 'U968581d6ef8ed6640f5383f62999a9e1';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('คำถามวันนี้คือ อะไรเอ่ย สี่ตีนเดินมาง หลังคามุมกระเบื้อง ใครรู้คำตอบบ้างเอ่ย');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
+$response2 = $bot->pushMessage($pushID2, $textMessageBuilder);
+echo $response2->getHTTPStatus() . ' ' . $response2->getRawBody();
 
 
 
